62da0bb6634dd37f93556e2136fdca1c497ab663
Nicholas Pedraza <nick.pedraza1@gmail.com>
2022-12-06 09:13:33 -0600

    Product manager 1

---
+++ fullstack/productmanager/client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductForm from './components/ProductForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductForm/>
+    </div>
+  );
+}
+
+export default App;
